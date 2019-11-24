7498bffb511f18f7255e425e33e0733b030f34b1
tgaur50 <55955008+tgaur50@users.noreply.github.com>
2019-11-24 23:10:39 +0530

    Delete index.js

--- slideSimple/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".slider").simpleSlider();
-	var slider = $(".slider").data("simpleslider");
-
-	slider.nextSlide(); // Go to the next slide
-	slider.prevSlide(); // Go to the previous slide
-	slider.nextSlide(slidenumber); // Go to the given slide
-});
